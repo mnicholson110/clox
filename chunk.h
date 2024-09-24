dc5945ad11f0d8d2d6a68efcdc2abe2ebe95e0b2
matt <mnicholson110@gmail.com>
2024-09-24 18:24:56 -0500

    14.1-14.5

--- chunk.h
+++ chunk.h
@@ -2,15 +2,23 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
-typedef enum
-{
+typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
-typedef struct
-{
-    uint8_t *code;
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+    ValueArray constants;
 } Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
