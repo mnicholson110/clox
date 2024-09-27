98065f75a84dc6557f363237e461619e47df2d56
matt <mnicholson110@gmail.com>
2024-09-27 11:30:37 -0500

    stack impl; project restructure

--- src/debug.c
+++ src/debug.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#include "debug.h"
-#include "value.h"
+#include "../include/debug.h"
+#include "../include/value.h"
 
 void disassembleChunk(Chunk* chunk, const char* name) {
     printf("== %s ==\n", name);
--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
-#include "chunk.h"
-#include "debug.h"
+#include "../include/chunk.h"
+#include "../include/vm.h"
 
 int main(int argc, const char* argv[]) {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
 
@@ -11,7 +12,8 @@ int main(int argc, const char* argv[]) {
 
     writeChunk(&chunk, OP_RETURN, 123);
 
-    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
