9944dc9d523a91ec1b751c42087686406d0bb666
matt <mnicholson110@gmail.com>
2024-10-29 17:37:22 -0500

    thru 20.4.5

--- src/table.c
+++ src/table.c
@@ -31,6 +31,20 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
     index = (index + 1) % capacity;
 }
 
+bool tableGet(Table* table, ObjString* key, Value* value) {
+    if (table->count == 0) {
+        return false;
+    }
+
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+    if (entry->key == NULL) {
+        return false;
+    }
+
+    *value = entry->value;
+    return true;
+}
+
 static void adjustCapacity(Table* table, int capacity) {
     Entry* entries = ALLOCATE(Entry, capacity);
     for (int i = 0; i < capacity; i++) {
